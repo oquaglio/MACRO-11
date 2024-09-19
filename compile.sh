a6f344d2234eb46db8f7e8d7e557a5c274622dc3
Otto Quaglio <otto.q@gmx.com>
2024-09-19 17:21:58 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
-OFLAGS="-Ofast -march=native -mtune=native -funroll-loops -flto -fprofile-generate -fprofile-use"
+# Set gcc compilation flags
+GENERATE_PROFILE="-Ofast -march=native -mtune=native -funroll-loops -flto -fprofile-generate"
+COMPILE_WITH_PROFILE="-Ofast -march=native -mtune=native -funroll-loops -flto -fprofile-use"
 
-gcc --version
-gcc $OFLAGS sieve.c -o sieve
+# Compile the program with -fprofile-generate:
+gcc $GENERATE_PROFILE sieve.c -o sieve
+
+# Generate .gcda profiling data:
+sieve
+
+# Recompile using the profile data:
+gcc $COMPILE_WITH_PROFILE sieve.c -o sieve
